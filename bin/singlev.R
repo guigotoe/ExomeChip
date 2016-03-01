9506553e9e5c127e14f3e252153bc46e2c6739c9
Guillermo Torres <guigotoe@gmail.com>
2016-03-01 12:10:12 +0100

    updated

--- bin/singlev.R
+++ bin/singlev.R
@@ -12,22 +12,24 @@ require(qqman)
 df <- assoc[,c(13,2,3,9)]
 colnames(df) <- c("SNP","CHR","BP","P")
 df[df<1e-16] <- 1e-16
-manhattan(df,main = "Manhattan Plot", cex=0.5,cex.axis=0.8,ymax=18)
+manhattan(df,main = "Ageing ExomeChip Manhattan Plot", cex=0.5,cex.axis=0.8,ymax=18)
 qq(df$P, main = "Q-Q plot of ExomeChip p-values")
+tocheck <- assoc[assoc["P"]<=1e-5,]
+write.table(tocheck[12],paste(sv.dir,'tocheck_clusterplot.txt',sep="/"),row.names=F,col.names=F,quote=F,sep="\t")
+tocheck2 <- assoc[assoc["P"]<=1e-8,]
+write.table(tocheck2[12],paste(sv.dir,'signf_tocheck_clusterplot.txt',sep="/"),row.names=F,col.names=F,quote=F,sep="\t")
 
 
-assoc2 <- read.table('/home/torres/Documents/Projects/AffyChip/assoc_alle_individuen_60_neu.assoc',header=T,as.is=T)
-assoc2 <- assoc2[complete.cases(assoc2[,9]),]   # Removing rows containing NAs in pvalue
-df2 <- assoc2[,c(2,1,3,9)]
-df2[df2<1e-16] <- 1e-16
-manhattan(df2,main = "Manhattan Plot", cex=0.5,cex.axis=0.8,ymax=18)
-qq(df2$P, main = "Q-Q plot of ExomeChip p-values")
 
-
-####
-
+###
 tocheck <- assoc[assoc["P"]<=0.01,]
 write.table(tocheck[12],paste(sv.dir,'tocheck_clusterplot.txt',sep="/"),row.names=F,col.names=F,quote=F,sep="\t")
+#
+scores <- read.table('/home/torres/Documents/Projects/ExomeChip/results/02_16_QC/ClusPlot_filtering/tocheck_clusterplot.txt.scores',header=F,as.is=T)
+no <- scores[scores[2]!=1,1]
+nochrpos <- names[names$'exmID'%in%no,3]
+write.table(nochrpos,paste(sv.dir,'fail_clusterplot.txt',sep="/"),row.names=F,col.names=F,quote=F,sep="\t")
+###
 
 
 write.table(assoc,paste(sv.dir,'assoc.txt',sep="/"),row.names=F,col.names=T,quote=F,sep="\t")
